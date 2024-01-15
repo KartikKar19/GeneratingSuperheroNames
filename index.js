c6266556a69f98d07fce054b7617a0c48d09293c
Kartik Kar <115065181+KartikKar19@users.noreply.github.com>
2024-01-16 03:09:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var generateName = require('sillyname');
+var sillyName = generateName();
+console.log(`My name is ${sillyName}.`);
+
+const superheroes = require('superheroes');
+superheroes.all;
+var heroList = superheroes.random();
+console.log(`I am ${heroList}`);
